ef496328465ebf7e92880b4f51bc9d900d69fb46
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-07-28 21:14:12 +0200

    finish javascript logi

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,18 @@
 const circle = document.getElementById('circle')
 let state = false
+let prices = document.getElementsByClassName('prices')
 
 const changePrice = () => {
-    if(!state) {
+    if(state) {
         state = false
-        console.log('price is monthly')
+        prices[0].innerHTML = `<span>&dollar;</span>199.99`
+        prices[1].innerHTML = `<span>&dollar;</span>249.99`
+        prices[2].innerHTML = `<span>&dollar;</span>399.99`
     } else {
         state = true
-        console.log('price is annual')
+        prices[0].innerHTML = `<span>&dollar;</span>19.99`
+        prices[1].innerHTML = `<span>&dollar;</span>24.99`
+        prices[2].innerHTML = `<span>&dollar;</span>39.99`
     }
 }
 
